fab8a751c2bf337dc510b93e4f2f444001ad700e
Yannis Liapis <yliapis@yanniss-air.lan>
2024-09-01 13:07:25 -0500

    update brew setup

--- install.sh
+++ install.sh
@@ -3,18 +3,32 @@
 
 echo "begining dotfiles install"
 
-
 # platform guard
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "macos detected"
+  DEFAULT_PROFILE_FILE="$HOME/.zprofile"
 else
   echo "unsupported platform detected: $OSTYPE"
   exit 1
 fi
 
+if [[ -f $DEFAULT_PROFILE_FILE ]]; then
+  echo "profile file exists"
+else
+  echo "profile file does not exist, creating"
+  touch $DEFAULT_PROFILE_FILE
+fi
 
 # install homebrew
 if ! command -v brew &> /dev/null; then
   echo "installing homebrew"
-  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  # install
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # add to profile and activate
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $DEFAULT_PROFILE_FILE
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+# turn off homebrew analytics
+# https://docs.brew.sh/Analytics
+brew analytics off
